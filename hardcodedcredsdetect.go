265fc58bbcb335fe8f8f578b3c4660aeb694abb3
kyosu-1 <sho013039@gmail.com>
2023-03-17 11:38:20 +0900

    go fmt

--- hardcodedcredsdetect.go
+++ hardcodedcredsdetect.go
@@ -1,54 +1,53 @@
 package hardcodedcredsdetect
 
-
 import (
-    "go/ast"
-    "go/token"
-    "regexp"
-    "strconv"
+	"go/ast"
+	"go/token"
+	"regexp"
+	"strconv"
 
-    "golang.org/x/tools/go/analysis"
-    "golang.org/x/tools/go/analysis/passes/inspect"
-    "golang.org/x/tools/go/ast/inspector"
+	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/passes/inspect"
+	"golang.org/x/tools/go/ast/inspector"
 )
 
 var (
-    passwordRegexp    = regexp.MustCompile(`(?i)password|passwd|pwd`)
-    credentialRegexp  = regexp.MustCompile(`(?i)credential|cred|auth.*token|api.*key`)
+	passwordRegexp   = regexp.MustCompile(`(?i)password|passwd|pwd`)
+	credentialRegexp = regexp.MustCompile(`(?i)credential|cred|auth.*token|api.*key`)
 )
 
 var Analyzer = &analysis.Analyzer{
-    Name: "hardcodedcredsdetect",
-    Doc:  "detects hardcoded credentials in code",
-    Run:  run,
-    Requires: []*analysis.Analyzer{
-        inspect.Analyzer,
-    },
+	Name: "hardcodedcredsdetect",
+	Doc:  "detects hardcoded credentials in code",
+	Run:  run,
+	Requires: []*analysis.Analyzer{
+		inspect.Analyzer,
+	},
 }
 
 func run(pass *analysis.Pass) (interface{}, error) {
-    var err error
-    astInsp := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+	var err error
+	astInsp := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
 
-    astInsp.Preorder(nil, func(n ast.Node) {
-        switch n := n.(type) {
-        case *ast.AssignStmt:
-            for i, expr := range n.Rhs {
-                if lit, ok := expr.(*ast.BasicLit); ok && lit.Kind == token.STRING {
-                    if isSensitiveVariableName(n.Lhs[i].(*ast.Ident).Name) {
-                        if value, err := strconv.Unquote(lit.Value); err == nil {
-                            pass.Reportf(lit.Pos(), "hardcoded credential found: %s", value)
-                        }
-                    }
-                }
-            }
-        }
-    })
+	astInsp.Preorder(nil, func(n ast.Node) {
+		switch n := n.(type) {
+		case *ast.AssignStmt:
+			for i, expr := range n.Rhs {
+				if lit, ok := expr.(*ast.BasicLit); ok && lit.Kind == token.STRING {
+					if isSensitiveVariableName(n.Lhs[i].(*ast.Ident).Name) {
+						if value, err := strconv.Unquote(lit.Value); err == nil {
+							pass.Reportf(lit.Pos(), "hardcoded credential found: %s", value)
+						}
+					}
+				}
+			}
+		}
+	})
 
-    return nil, err
+	return nil, err
 }
 
 // judge whether the variable name is sensitive or not
 func isSensitiveVariableName(name string) bool {
-    return passwordRegexp.MatchString(name) || credentialRegexp.MatchString(name)
+	return passwordRegexp.MatchString(name) || credentialRegexp.MatchString(name)
 }
